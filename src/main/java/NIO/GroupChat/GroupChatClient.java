ff4bf2b3e15f6270c491c8134705a8fee22863ab
Administrator <929874725@qq.com>
2020-07-31 17:05:39 +0800

    nio多人聊天

---
+++ src/main/java/NIO/GroupChat/GroupChatClient.java
@@ -0,0 +1,78 @@
+package NIO.GroupChat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class GroupChatClient {
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private static final String HOSTNAME="127.0.0.1";
+    private static final int PORT=6667;
+    private String userName;
+    public GroupChatClient() throws IOException {
+        selector=Selector.open();
+        socketChannel=SocketChannel.open(new InetSocketAddress(HOSTNAME,PORT));
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_READ);
+        userName=socketChannel.getLocalAddress().toString();
+        System.out.println(userName+" is ok....");
+    }
+    public void sendMsg(String msg){
+        String info =userName+" 说："+msg;
+        try {
+            socketChannel.write(ByteBuffer.wrap(info.getBytes()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void readMsg(){
+        try {
+            int readChannel = selector.select();
+            if(readChannel>0){
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()){
+                    SelectionKey key = iterator.next();
+                    if(key.isReadable()){
+                        SocketChannel channel = (SocketChannel)key.channel();
+                        channel.configureBlocking(false);
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        channel.read(buffer);
+                        System.out.println(new String(buffer.array()));
+                    }
+                    iterator.remove();
+                }
+            }else {
+                System.out.println("没有可用的通道");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        GroupChatClient client = new GroupChatClient();
+        new Thread(()->{
+            while (true){
+            client.readMsg();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }}
+        }).start();
+        new Thread(()-> {
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()){
+                String info = scanner.next();
+                client.sendMsg(info);
+            }
+        }
+        ).start();
+    }
+}
